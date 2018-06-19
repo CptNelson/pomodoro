e52f3e9f7b5e7d7f0788ae346693afcadc19441d
CptNelson <toni.lappalainen@gmail.com>
2018-06-19 23:00:08 +0300

    wrapped into an object

--- src/pomodoro.js
+++ src/pomodoro.js
@@ -1,79 +1,61 @@
 
-
-
-let clock = {
-
+let pomodoro = {
     minutes : null,
     seconds : null,
-
-    getMinutes : function() {
-        return minutes;
-    },
-    setMinutes : function(min) {
-        minutes = min;
-    },
-    getSeconds : function () {
-        return seconds;
-    },
-    setSeconds : function(sec) {
-        seconds = sec;
+    minutesDOM : document.getElementById("minutes"),
+    secondsDOM : document.getElementById("seconds"),
+    startButton : document.getElementById("startButton"),
+    alarmSound : new Audio('src/alarm.mp3'),
+    init : function() {
+        this.minutes = 25;
+        this.seconds = 0;
+        this.setClockDOM();        
+        startButton.onclick = function() {
+            this.disabled = true;
+            pomodoro.countdown();
+            startButton.style.color = 'grey';
     }
-}
-
-function setClockDOM() {
-    minStr = clock.getMinutes().toString();
-    secStr = clock.getSeconds().toString();
-    if (clock.getMinutes() < 10) {
-        minStr = "0" + minStr;
-    }
-    if (clock.getSeconds() < 10) {
-        secStr = "0" + secStr;
-    } 
-
-    minutesDOM = document.getElementById("minutes");
-    minutesDOM.childNodes[0].textContent = minStr;
-    secondsDOM = document.getElementById("seconds");
-    secondsDOM.childNodes[0].textContent = secStr;
-}
-
-function countdown() {
-    let min = clock.getMinutes();
-    let sec = clock.getSeconds();
-        if (sec == 0)
-        sec = 59;
-    let timerInterval = setInterval(function() {
-        clock.setSeconds(sec);
-        clock.setMinutes(min);
-        setClockDOM();
-        sec = sec - 1;
-        if (sec < 0) {
-            min -= 1;
-            if (min < 0) { 
-                clearInterval(timerInterval);
-                ready();
-            }
-            sec = 59; 
+    },
+    setClockDOM : function() {
+        minStr = this.minutes.toString();
+        secStr = this.seconds.toString();
+        if (this.minutes < 10) {
+            minStr = "0" + minStr;
         }
-    }, 1000);
-} 
+        if (this.seconds < 10) {
+            secStr = "0" + secStr;
+        } 
 
-function ready() {
-    console.log("ready!")
-    alarmSound.play();
-}
-
-
-function init () {
-    alarmSound = new Audio('src/alarm.mp3');
-    clock.setMinutes(25);
-    clock.setSeconds(0);
-    setClockDOM();
+        this.minutesDOM.childNodes[0].textContent = minStr;
+        this.secondsDOM.childNodes[0].textContent = secStr;
+    },
+    countdown : function() {
+        let self = this;
+        let min = self.minutes - 1;
+        let sec = self.seconds;
+            if (sec == 0)
+            sec = 59;
+        let timerInterval = setInterval(function() {
+            self.seconds = sec;
+            self.minutes = min;
+            sec = sec - 1;
+            if (sec < 0) {
+                min -= 1;
+                if (min < 0) { 
+                    clearInterval(timerInterval);
+                    this.ready();
+                }
+                sec = 59; 
+            }
+            self.setClockDOM();
+        }, 1000);
+    }, 
+    ready : function() {
+        console.log("ready!")
+        alarmSound.play();
+    }
 }
 
 window.onload = function(){
-    init();
-    let startButton = document.getElementById("startButton");
-    startButton.onclick = function() {
-        countdown();
-    }
+    pomodoro.init();
   };
