2f1c05a285680d51838dd101efe4558f28743185
CptNelson <toni.lappalainen@gmail.com>
2019-03-17 11:39:45 +0200

    adding and decreasing

--- src/pomodoro.js
+++ src/pomodoro.js
@@ -2,19 +2,74 @@
 let pomodoro = {
     minutes : null,
     seconds : null,
+    savedMin : null,
+    savedSec : null,
+    timerInterval : null,
     minutesDOM : document.getElementById("minutes"),
     secondsDOM : document.getElementById("seconds"),
     startButton : document.getElementById("startButton"),
+    addOne : document.getElementById("addOne"),
+    addFive : document.getElementById("addFive"),
+    decOne : document.getElementById("decOne"),
+    decFive : document.getElementById("decFive"),
     alarmSound : new Audio('src/alarm.mp3'),
     init : function() {
         this.minutes = 25;
         this.seconds = 0;
-        this.setClockDOM();        
+        this.setClockDOM();
+        startButton.state = 0;
+        
+        addOne.onclick = function() {
+            pomodoro.minutes += 1;
+            pomodoro.setClockDOM();
+        }
+        
+        addFive.onclick = function() {
+            pomodoro.minutes += 5;
+            pomodoro.setClockDOM();
+        }
+        decOne.onclick = function() {
+            pomodoro.minutes -= 1;
+            pomodoro.setClockDOM();
+        }
+        decFive.onclick = function() {
+            pomodoro.minutes -= 5;
+            pomodoro.setClockDOM();
+        }
+
         startButton.onclick = function() {
-            this.disabled = true;
-            pomodoro.countdown();
-            startButton.style.color = 'grey';
-    }
+            if (this.state == 0) {
+                this.textContent = "reset"
+                pomodoro.savedMin = pomodoro.minutes;
+                pomodoro.savedSec = pomodoro.seconds;
+                pomodoro.countdown();
+                addOne.style.color = 'grey';
+                decOne.style.color = 'grey';
+                addFive.style.color = 'grey';
+                decFive.style.color = 'grey';
+                addOne.disabled = true;
+                decOne.disabled = true;
+                addFive.disabled = true;
+                decFive.disabled = true;
+                this.state = 1;
+            } else {
+                this.textContent = "start"
+                pomodoro.minutes = pomodoro.savedMin;
+                pomodoro.seconds = pomodoro.savedSec;
+                startButton.style.color = 'black';
+                addOne.style.color = 'black';
+                decOne.style.color = 'black';
+                addFive.style.color = 'black';
+                decFive.style.color = 'black';
+                addOne.disabled = false;
+                decOne.disabled = false;
+                addFive.disabled = false;
+                decFive.disabled = false;
+                clearInterval(timerInterval);
+                pomodoro.setClockDOM();
+                this.state = 0;
+            }
+        }
     },
     setClockDOM : function() {
         minStr = this.minutes.toString();
@@ -35,7 +90,7 @@ let pomodoro = {
         let sec = self.seconds;
             if (sec == 0)
             sec = 59;
-        let timerInterval = setInterval(function() {
+        timerInterval = setInterval(function() {
             self.seconds = sec;
             self.minutes = min;
             sec = sec - 1;
